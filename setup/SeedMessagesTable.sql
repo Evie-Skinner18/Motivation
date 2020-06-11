6223b516b56c2223f4afc29a7bacd151de2fdf11
Evie Skinner <evie.skinner@teamriver.com>
2020-06-11 13:56:43 +0100

    Created setup dir with create DB and seed Messages table scripts

---
+++ setup/SeedMessagesTable.sql
@@ -0,0 +1,8 @@
+use Motivation
+
+
+INSERT INTO Messages(Text, Author, ImageUrl)
+VALUES
+	('Be the change you want to see in the world', 'Gandhi', 'https://www.sbs.com.au/yourlanguage/sites/sbs.com.au.yourlanguage/files/ghandi.jpg'),
+	('I paid the cost to be the boss', 'James Brown', 'https://pmcvariety.files.wordpress.com/2014/09/james-brown.jpg?w=1000&h=563&crop=1'),
+	('Hard work beats talent when talent doesnt work hard', 'Tim Notke', 'https://i.pinimg.com/originals/b7/a7/06/b7a706df61578c0b91fd7b648ff80e7c.jpg');
